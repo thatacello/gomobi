8ff6d943fc57b56269c15bfb774b74d5e08b9e3e
Thais Cardoso <farias.thais@uol.com.br>
2022-11-07 01:14:40 -0300

    start perfil

--- src/pages/perfil/index.tsx
+++ src/pages/perfil/index.tsx
@@ -3,18 +3,41 @@ import './styles.css';
 import HomeIcon from '../../assets/icons/home.svg';
 import AtividadeIcon from '../../assets/icons/atividade.svg';
 import PerfilIcon from '../../assets/icons/perfil.svg';
-import Centauro from '../../assets/images/centauro.png';
-import Decathlon from '../../assets/images/decathlon.png';
-import Netshoes from '../../assets/images/netshoes.png';
-import Desafio1 from '../../assets/images/desafio1.png';
-import Desafio2 from '../../assets/images/desafio2.png';
-import Desafio3 from '../../assets/images/desafio3.png';
+import Estimulo1 from '../../assets/images/estimulo1.png';
+import Estimulo2 from '../../assets/images/estimulo2.png';
+import Estimulo3 from '../../assets/images/estimulo3.png';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 function Perfil() {
+  const handleDragStart = (e: any) => e.preventDefault();
+
+  const items = [
+    <img
+      className="brands"
+      src={Estimulo1}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt=""
+    />,
+    <img
+      className="brands"
+      src={Estimulo2}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt=""
+    />,
+    <img
+      className="brands"
+      src={Estimulo3}
+      onDragStart={handleDragStart}
+      role="presentation"
+      alt=""
+    />,
+  ];
+
   return (
-    <Container className='external'>
+    <Container className="external">
       <Row>
         <Navbar variant="light">
           <Container className="navbar-container">
@@ -38,6 +61,42 @@ function Perfil() {
             </Nav>
           </Container>
         </Navbar>
+      </Row>
+
+      <Row className="d-flex align-items-center">
+        <Col sm={12}>
+          <h2 className="contamos">Histórico de atividades</h2>
+          <AliceCarousel
+            mouseTracking
+            items={items}
+            autoPlay={true}
+            infinite={true}
+            animationDuration={5000}
+            autoPlayStrategy="none"
+            disableButtonsControls={true}
+            disableDotsControls={true}
+            responsive={{
+              0: {
+                items: 1,
+              },
+              625: {
+                items: 2,
+              },
+              1024: {
+                items: 3,
+              },
+            }}
+          />
+        </Col>
+      </Row>
+
+      <Row className="d-flex align-items-center">
+        <Col sm={12}>
+          <h2 className="contamos">Conquistas</h2>
+          <div className='conquistas-container'>
+            
+          </div>
+        </Col>
       </Row>
     </Container>
   );
